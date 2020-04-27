ed8fe231496f05fc7b14c166f6ed8fca318c7ac2
knwch <knwch@harmoinc.local>
2020-04-28 02:36:26 +0700

    deleted lints inline

--- App.js
+++ App.js
@@ -1,4 +1,3 @@
-/* eslint-disable react-native/no-inline-styles */
 import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
