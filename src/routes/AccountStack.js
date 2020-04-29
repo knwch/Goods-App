bc05a017033d53e2e7aec4de008c9d0ccfc09220
knwch <knwch@harmoinc.local>
2020-04-29 21:36:00 +0700

    update auth stack

--- src/routes/AccountStack.js
+++ src/routes/AccountStack.js
@@ -1,13 +1,53 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import Account from '../screens/Account';
+import Signup from '../screens/Signup';
+import Signin from '../screens/Signin';
 
 const AccountStack = createStackNavigator();
 
-export default function AccountStackScreen() {
-  return (
-    <AccountStack.Navigator initialRouteName="Account">
-      <AccountStack.Screen name="Account" component={Account} />
-    </AccountStack.Navigator>
-  );
+export default class AccountStackScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      typedata: ['Developer', 'Designer', 'Product Manager'],
+      userToken: null,
+    };
+  }
+
+  render() {
+    return (
+      <AccountStack.Navigator
+        initialRouteName="Lists"
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: '#2c3d70',
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+          headerTitleAlign: 'center',
+        }}>
+        {this.state.userToken == null ? (
+          <>
+            <AccountStack.Screen
+              name="Signin"
+              component={Signin}
+              options={{title: 'Signin'}}
+            />
+            <AccountStack.Screen
+              name="Signup"
+              component={Signup}
+              options={{title: 'Signup'}}
+            />
+          </>
+        ) : (
+          <>
+            <AccountStack.Screen name="Account" component={Account} />
+          </>
+        )}
+      </AccountStack.Navigator>
+    );
+  }
 }
