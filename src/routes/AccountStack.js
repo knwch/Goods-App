b580bf5ce61dbd9125146dd035283642c29624ed
knwch <knwch@harmoinc.local>
2020-04-30 16:04:54 +0700

    removed ApplicationProvider

--- src/routes/AccountStack.js
+++ src/routes/AccountStack.js
@@ -3,8 +3,6 @@ import {createStackNavigator} from '@react-navigation/stack';
 import Account from '../screens/Account';
 import Signup from '../screens/Signup';
 import Signin from '../screens/Signin';
-import {ApplicationProvider} from '@ui-kitten/components';
-import * as eva from '@eva-design/eva';
 
 const AccountStack = createStackNavigator();
 
@@ -19,39 +17,37 @@ export default class AccountStackScreen extends Component {
 
   render() {
     return (
-      <ApplicationProvider {...eva} theme={eva.light}>
-        <AccountStack.Navigator
-          initialRouteName="Lists"
-          screenOptions={{
-            headerStyle: {
-              backgroundColor: '#2c3d70',
-            },
-            headerTintColor: '#fff',
-            headerTitleStyle: {
-              fontWeight: 'bold',
-            },
-            headerTitleAlign: 'center',
-          }}>
-          {this.state.userToken == null ? (
-            <>
-              <AccountStack.Screen
-                name="Signin"
-                component={Signin}
-                options={{title: 'Signin'}}
-              />
-              <AccountStack.Screen
-                name="Signup"
-                component={Signup}
-                options={{title: 'Signup'}}
-              />
-            </>
-          ) : (
-            <>
-              <AccountStack.Screen name="Account" component={Account} />
-            </>
-          )}
-        </AccountStack.Navigator>
-      </ApplicationProvider>
+      <AccountStack.Navigator
+        initialRouteName="Lists"
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: '#2c3d70',
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+          headerTitleAlign: 'center',
+        }}>
+        {this.state.userToken == null ? (
+          <>
+            <AccountStack.Screen
+              name="Signin"
+              component={Signin}
+              options={{title: 'Signin'}}
+            />
+            <AccountStack.Screen
+              name="Signup"
+              component={Signup}
+              options={{title: 'Signup'}}
+            />
+          </>
+        ) : (
+          <>
+            <AccountStack.Screen name="Account" component={Account} />
+          </>
+        )}
+      </AccountStack.Navigator>
     );
   }
 }
--- src/routes/HomeStack.js
+++ src/routes/HomeStack.js
@@ -3,46 +3,42 @@ import {createStackNavigator} from '@react-navigation/stack';
 import {Image, StyleSheet, View} from 'react-native';
 import Home from '../screens/Home';
 import Map from '../screens/Map';
-import {ApplicationProvider} from '@ui-kitten/components';
-import * as eva from '@eva-design/eva';
 
 const HomeStack = createStackNavigator();
 
 export default class HomeStackScreen extends React.Component {
   render() {
     return (
-      <ApplicationProvider {...eva} theme={eva.light}>
-        <HomeStack.Navigator
-          initialRouteName="Home"
-          screenOptions={{
-            headerStyle: {
-              backgroundColor: '#2c3d70',
-              height: 90,
-            },
-            headerTintColor: '#fff',
-            headerTitleStyle: {
-              fontWeight: 'bold',
-            },
-            headerTitleAlign: 'center',
-            headerTitle: (
-              //<View>
-              <Image
-                style={styles.image}
-                source={require('../assets/goods-white.png')}
-              />
-              //</View>
-            ),
-          }}>
-          <HomeStack.Screen name="Home" component={Home} />
-          <HomeStack.Screen
-            name="Map"
-            component={Map}
-            options={{
-              headerBackTitle: ' ',
-            }}
-          />
-        </HomeStack.Navigator>
-      </ApplicationProvider>
+      <HomeStack.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: '#2c3d70',
+            height: 90,
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+          headerTitleAlign: 'center',
+          // headerTitle: (
+          //   //<View>
+          //   <Image
+          //     style={styles.image}
+          //     source={require('../assets/goods-white.png')}
+          //   />
+          //   //</View>
+          // ),
+        }}>
+        <HomeStack.Screen name="Home" component={Home} />
+        <HomeStack.Screen
+          name="Map"
+          component={Map}
+          options={{
+            headerBackTitle: ' ',
+          }}
+        />
+      </HomeStack.Navigator>
     );
   }
 }
