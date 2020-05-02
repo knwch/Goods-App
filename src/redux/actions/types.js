435667a47f5bdf65a66325cb4f83758af42c26a1
famesensor <famfew25@gmail.com>
2020-05-03 00:40:21 +0700

    [update] add clear error

--- src/redux/actions/types.js
+++ src/redux/actions/types.js
@@ -2,3 +2,4 @@ export const SET_CURRENT_USER = 'SIGN_IN';
 export const GET_ERRORS = 'GET_ERRORS';
 export const SET_LOADING = 'SET_LOADING';
 export const UN_LOADING = 'UN_LOADING';
+export const CLEAR_ERRORS = 'CLEAR_ERRORS';
