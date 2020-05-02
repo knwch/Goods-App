6f4fe4546d3ee60d35b6d657b4cea2489da8ab7b
famesensor <famfew25@gmail.com>
2020-05-02 17:34:31 +0700

    [update] add get token from localstorage

--- App.js
+++ App.js
@@ -8,56 +8,86 @@ import * as eva from '@eva-design/eva';
 import GoodStack from './src/routes/GoodStack';
 import HomeStack from './src/routes/HomeStack';
 import AccountStack from './src/routes/AccountStack';
-
+import store from './src/redux/store';
+import * as Keychain from 'react-native-keychain';
+import setAuthToken from './src/utils/setAuthToken';
+import jwt_decode from 'jwt-decode';
+import {setCurrentUser, signoutUser} from './src/redux/actions/authActions';
 Ionicons.loadFont();
 
 const Tab = createBottomTabNavigator();
 
+const getToken = async () => {
+  try {
+    console.log('function');
+    const token = await Keychain.getGenericPassword();
+    if (token) {
+      setAuthToken(token);
+      const decoded = jwt_decode(token);
+      store.dispatch(setCurrentUser(decoded));
+      if (decoded.exp < Date.now() / 1000) {
+        store.dispatch(signoutUser());
+      }
+    } else {
+      console.log('login');
+    }
+  } catch (err) {
+    console.error(err);
+  }
+};
+
 export default function App() {
+  React.useEffect(() => {
+    console.log('componentwillmount');
+    getToken();
+  }, []);
+
   return (
     <ApplicationProvider {...eva} theme={eva.light}>
-      {/* <Provider store={store}> */}
-      <NavigationContainer>
-        <Tab.Navigator
-          screenOptions={({route}) => ({
-            tabBarIcon: ({focused, color, size}) => {
-              if (route.name === 'Home') {
-                return (
-                  <Ionicons
-                    name={focused ? 'home' : 'home-outline'}
-                    size={size}
-                    color={color}
-                  />
-                );
-              } else if (route.name === 'My Goods') {
-                return (
-                  <Ionicons
-                    name={focused ? 'cube' : 'cube-outline'}
-                    size={size}
-                    color={color}
-                  />
-                );
-              } else if (route.name === 'Account') {
-                return (
-                  <Ionicons
-                    name={focused ? 'account-circle' : 'account-circle-outline'}
-                    size={size}
-                    color={color}
-                  />
-                );
-              }
-            },
-          })}
-          tabBarOptions={{
-            activeTintColor: 'rgb(33,50,99)',
-            inactiveTintColor: 'gray',
-          }}>
-          <Tab.Screen name="Home" component={HomeStack} />
-          <Tab.Screen name="My Goods" component={GoodStack} />
-          <Tab.Screen name="Account" component={AccountStack} />
-        </Tab.Navigator>
-      </NavigationContainer>
-      {/* </Provider> */}
+      <Provider store={store}>
+        <NavigationContainer>
+          <Tab.Navigator
+            screenOptions={({route}) => ({
+              tabBarIcon: ({focused, color, size}) => {
+                if (route.name === 'Home') {
+                  return (
+                    <Ionicons
+                      name={focused ? 'home' : 'home-outline'}
+                      size={size}
+                      color={color}
+                    />
+                  );
+                } else if (route.name === 'My Goods') {
+                  return (
+                    <Ionicons
+                      name={focused ? 'cube' : 'cube-outline'}
+                      size={size}
+                      color={color}
+                    />
+                  );
+                } else if (route.name === 'Account') {
+                  return (
+                    <Ionicons
+                      name={
+                        focused ? 'account-circle' : 'account-circle-outline'
+                      }
+                      size={size}
+                      color={color}
+                    />
+                  );
+                }
+              },
+            })}
+            tabBarOptions={{
+              activeTintColor: 'rgb(33,50,99)',
+              inactiveTintColor: 'gray',
+            }}>
+            <Tab.Screen name="Home" component={HomeStack} />
+            <Tab.Screen name="My Goods" component={GoodStack} />
+            <Tab.Screen name="Account" component={AccountStack} />
+          </Tab.Navigator>
+        </NavigationContainer>
+      </Provider>
     </ApplicationProvider>
   );
 }
